af673f648155f5af88f0a5b15d098fd8aa690277
Adeel Harry <60717148+AHarry7@users.noreply.github.com>
2022-08-09 23:16:48 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Movies from "./components/movies";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <main className="container">
+          <Movies />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
