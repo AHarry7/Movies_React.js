b75dabb470b5fdce3e49754f4d078f53d36dbcda
Adeel Harry <60717148+AHarry7@users.noreply.github.com>
2022-08-15 12:01:32 -0700

    Update movies.jsx

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../Services/fakeMoviesService.js";
-
+import Like from "./common/like.jsx";
 class Movies extends Component {
   state = {
     movies: getMovies(),
@@ -10,31 +10,43 @@ class Movies extends Component {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies: movies });
   };
+
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movie };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
   render() {
     const { length: count } = this.state.movies;
     if (count === 0)
-      return <h5
+      return (
+        <h5
           style={{ marginLeft: "auto", marginRight: "auto", display: "table" }}
           className="mb-4"
         >
           There is no movie in the database
-        </h5>;
+        </h5>
+      );
     return (
       <React.Fragment>
-       <h5
+        <h5
           style={{ marginLeft: "auto", marginRight: "auto", display: "table" }}
           className="mb-4"
         >
-            Showing {count} movies in the database
+          Showing {count} movies in the database
         </h5>
-      <table className="table">
+        <table className="table">
           <thead>
             <tr>
               <th>Title</th>
               <th>Genre</th>
               <th>Stock</th>
               <th>Rate</th>
-              <th></th>
+              <th />
+              <th />
             </tr>
           </thead>
           <tbody>
@@ -44,6 +56,13 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    onClick={this.handleLike}
+                    liked={movie.liked}
+                    movie={movie}
+                  />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
